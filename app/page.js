9416811677d0de0e6e276d4b2b6f92a1bc2fe79d
Anjali <anjalisah89@gmail.com>
2024-04-03 00:00:21 +0530

    Update

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import "@/app/globals.css";
+import Register from './register';
+
+export default function Home() {
+  return (
+   <div>
+     <Register />
+   </div>
+  );
+}
--- authsetup/src/app/page.js
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import "./globals.css";
-
-export default function Home() {
-  return (
-   <div></div>
-  );
-}
